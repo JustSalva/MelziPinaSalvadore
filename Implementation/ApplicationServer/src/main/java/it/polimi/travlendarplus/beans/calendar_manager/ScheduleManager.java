2a99b27c2545d5e7e83ede0946284a0c08cbfe91
Unknown <pietromelzi95@gmail.com>
2017-12-03 23:34:34 +0100

    As below

--- Implementation/ApplicationServer/src/main/java/it/polimi/travlendarplus/beans/calendar_manager/ScheduleManager.java
+++ Implementation/ApplicationServer/src/main/java/it/polimi/travlendarplus/beans/calendar_manager/ScheduleManager.java
@@ -25,21 +25,40 @@ public class ScheduleManager extends UserManager{
     private Long startingTime;
     private Long endingTime;
     private List<Event> schedule;
-    private List<Event> eventToSwapOut;
     private Event eventToSwapIn;
+    private List<Event> eventToSwapOut;
 
-    public void setStartingTime(Long startingTime) {
+    public void setTime(Long startingTime) {
         this.startingTime = startingTime;
-        this.endingTime = startingTime + 24*60*60*999;
+        this.endingTime = startingTime + 24*60*59;
     }
 
     public List<Event> getSchedule() {
+        setSchedule();
         return Collections.unmodifiableList(schedule);
     }
 
-    public void setSchedule() {
+    private void setSchedule() {
         String query = "SELECT * FROM Event WHERE User = "+getCurrentUser().getEmail()+
-                " AND starting time ecc";
+                " AND starting time ecc"+startingTime+endingTime;
+        // *execute query and handle results *
         this.schedule = schedule;
     }
+
+    public void setEventToSwapIn(Event eventToSwapIn) {
+        this.eventToSwapIn = eventToSwapIn;
+        setEventToSwapOut();
+    }
+
+    private void setEventToSwapOut() {
+        //query based on EventToSwapIn
+    }
+
+    public List<Event> swapEvents() {
+        // queries that act on swapIn and swapOut events
+        setTime(eventToSwapIn.calculateDate());
+        return getSchedule();
+    }
+
+    //TODO decide if for any operation we want attributes automatically updated or we have to call the relative function in order to obtain it
 }
