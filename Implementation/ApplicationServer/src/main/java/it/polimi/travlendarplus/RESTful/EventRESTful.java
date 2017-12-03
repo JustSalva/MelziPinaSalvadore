068428b1315ae5cf72ab0ef87a4d68833e9eeb47
Unknown <pietromelzi95@gmail.com>
2017-12-03 13:17:55 +0100

    RESTful definition of functions

--- Implementation/ApplicationServer/src/main/java/it/polimi/travlendarplus/RESTful/EventRESTful.java
+++ Implementation/ApplicationServer/src/main/java/it/polimi/travlendarplus/RESTful/EventRESTful.java
@@ -1,4 +1,35 @@
 package it.polimi.travlendarplus.RESTful;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+// The Java class will be hosted at the URI path "/schedule"
+@Path("/event")
 public class EventRESTful {
+
+    @Path("{idEvent}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JSONObject getEventInformation(@PathParam("idEvent") int id) {
+        return null;
+    }
+
+    @Path("{event}")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public boolean createEvent(@PathParam("event") JSONObject event) {
+        //this function return the result of the creation.
+        return false;
+    }
+
+    @Path("alternatives/{idEvent}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JSONArray getAlternatives(@PathParam("idEvent") int idEvent) {
+        return null;
+    }
+
 }
--- Implementation/ApplicationServer/src/main/java/it/polimi/travlendarplus/RESTful/PreferenceRESTful.java
+++ Implementation/ApplicationServer/src/main/java/it/polimi/travlendarplus/RESTful/PreferenceRESTful.java
@@ -1,4 +1,57 @@
 package it.polimi.travlendarplus.RESTful;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+// The Java class will be hosted at the URI path "/preference"
+@Path("/preference")
 public class PreferenceRESTful {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JSONArray getAllPreferences() {
+        return null;
+    }
+
+    @Path("{idToe}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JSONObject getTypeOfEvent(@PathParam("idToe") int id) {
+        return null;
+    }
+
+
+    @Path("{toe}")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public boolean createTypeOfEvent(@PathParam("toe")JSONObject toe) {
+        return false;
+    }
+
+    //this RESTfuls manage the preferred location of the user
+
+    @Path("location")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JSONArray getAllPreferredLocations() {
+        return null;
+    }
+
+    @Path("location/{idLoc}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JSONObject getPreferredLocation(@PathParam("idLoc") int idLoc) {
+        return null;
+    }
+
+
+    @Path("{loc}")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public boolean addPreferredLocation(@PathParam("loc")JSONObject loc) {
+        return false;
+    }
 }
