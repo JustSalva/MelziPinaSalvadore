4adfcccf1cc60a87cd4c8e6f4d2df906715ee27f
Alessandro Pina <pina.alessandro17@gmail.com>
2017-12-30 17:08:00 +0100

    Added onBack also to the map

--- Implementation/AndroidApp/Travlendar/app/src/main/java/it/polimi/travlendarplus/activity/MapsActivity.java
+++ Implementation/AndroidApp/Travlendar/app/src/main/java/it/polimi/travlendarplus/activity/MapsActivity.java
@@ -1,6 +1,9 @@
 package it.polimi.travlendarplus.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -51,5 +54,16 @@ public class MapsActivity extends MenuActivity implements OnMapReadyCallback {
         LatLng sydney = new LatLng( -34, 151 );
         mMap.addMarker( new MarkerOptions().position( sydney ).title( "Marker in Sydney" ) );
         mMap.moveCamera( CameraUpdateFactory.newLatLng( sydney ) );
+    }
+
+    @Override
+    public void onBackPressed () {
+        DrawerLayout drawer = findViewById( R.id.drawer_layout );
+        if ( drawer.isDrawerOpen( GravityCompat.START ) ) {
+            drawer.closeDrawer( GravityCompat.START );
+        } else {
+            super.onBackPressed();
+            startActivity( new Intent( this, CalendarActivity.class ) );
+        }
     }
 }
