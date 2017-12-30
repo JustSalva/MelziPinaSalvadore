f5a5874b43a5c85c09e200b742cb4341f4b6a886
Alessandro Pina <pina.alessandro17@gmail.com>
2017-12-30 18:40:12 +0100

    Map with locations

--- Implementation/AndroidApp/Travlendar/app/src/main/java/it/polimi/travlendarplus/activity/MapsActivity.java
+++ Implementation/AndroidApp/Travlendar/app/src/main/java/it/polimi/travlendarplus/activity/MapsActivity.java
@@ -1,47 +1,77 @@
 package it.polimi.travlendarplus.activity;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Looper;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import it.polimi.travlendarplus.Location;
 import it.polimi.travlendarplus.R;
+import it.polimi.travlendarplus.activity.handler.LocationLoader;
+import it.polimi.travlendarplus.activity.handler.location.GetLocationsHandler;
+import it.polimi.travlendarplus.database.view_model.UserViewModel;
+import it.polimi.travlendarplus.retrofit.controller.location.GetLocationsController;
 
 
 /**
- * Activity that shows a map containing the travel to be made by the user.
- * To be implemented, only a map is showed for now.
+ * Activity that shows a map containing the locations added by the user.
  **/
-// extends FragmentActivity
-public class MapsActivity extends MenuActivity implements OnMapReadyCallback {
+// extends MenuActivity
+public class MapsActivity extends FragmentActivity
+        implements OnMapReadyCallback, LocationLoader, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
+    private UserViewModel userViewModel;
+    private boolean locationsLoaded = false;
+    private Map<String, Location> locationMap;
+    private Map<String, Marker> markerMap;
+    private String token;
+    private GetLocationsHandler getLocationsHandler;
 
     @Override
     protected void onCreate ( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_maps );
-        super.setupMenuToolbar();
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = ( SupportMapFragment ) getSupportFragmentManager()
                 .findFragmentById( R.id.map_fragment );
         mapFragment.getMapAsync( this );
+
+        this.getLocationsHandler = new GetLocationsHandler(Looper.getMainLooper(), this);
+
+        userViewModel = ViewModelProviders.of( this ).get( UserViewModel.class );
+        userViewModel.getUser().observe( this, user -> {
+            token = user != null ? user.getToken() : "";
+            if ( token != null && !locationsLoaded ) {
+                // Server request for locations.
+                GetLocationsController getLocationsController = new GetLocationsController(getLocationsHandler);
+                getLocationsController.start(token);
+            }
+        } );
     }
 
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -50,10 +80,21 @@ public class MapsActivity extends MenuActivity implements OnMapReadyCallback {
     public void onMapReady ( GoogleMap googleMap ) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng( -34, 151 );
-        mMap.addMarker( new MarkerOptions().position( sydney ).title( "Marker in Sydney" ) );
-        mMap.moveCamera( CameraUpdateFactory.newLatLng( sydney ) );
+        // Set a listener for marker click.
+        mMap.setOnMarkerClickListener(this);
+    }
+
+    /** Called when the user clicks a marker. */
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+        Toast.makeText(this,
+                    marker.getTitle(),
+                    Toast.LENGTH_SHORT).show();
+
+        // Return false to indicate that we have not consumed the event and that we wish
+        // for the default behavior to occur (which is for the camera to move such that the
+        // marker is centered and for the marker's info window to open, if it has one).
+        return false;
     }
 
     @Override
@@ -64,6 +105,24 @@ public class MapsActivity extends MenuActivity implements OnMapReadyCallback {
         } else {
             super.onBackPressed();
             startActivity( new Intent( this, CalendarActivity.class ) );
+        }
+    }
+
+    @Override
+    public void updateLocations(Map<String, Location> locationMap) {
+        this.locationMap = locationMap;
+        this.markerMap = new HashMap<>();
+
+        // Add some markers to the map, and add a data object to each marker.
+        for (Location location : locationMap.values()) {
+            Marker marker = mMap.addMarker(new MarkerOptions()
+                    .position( new LatLng(
+                            location.getLocation().getLatitude(),
+                            location.getLocation().getLongitude()
+                    ) )
+                    .title(location.getName()));
+            marker.setTag(location.getName());
+            markerMap.put(location.getName(), marker);
         }
     }
 }
